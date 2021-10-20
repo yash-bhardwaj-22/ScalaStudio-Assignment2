d68ca6ac773b7f9c36d98baf6f2a04ec6d8e58b1
yash-bhardwaj-22 <yash.bhardwaj@knoldus.com>
2021-10-20 21:59:10 +0530

    First Commit

--- src/main/scala/edu/knoldus/Searching.scala
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -2,8 +2,27 @@ package edu.knoldus
 
 class Searching {
 
-  def binarySearch(array: Array[Int], elem: Int): Boolean = ???
+  def binarySearch(array: Array[Int], elem: Int): Boolean = {
+    def search(arr: Array[Int], target: Int, low: Int, high: Int): Int = {
 
-  def linearSearch(array: Array[Int], elem: Int): Boolean = ???
+      if (low > high) return -1
+      val middle = low + (high - low) / 2
+
+      arr match {
+        case(arr:Array[Int]) if (arr(middle) == target) => middle
+        case(arr:Array[Int]) if (arr(middle) < target) => search(arr, target, middle + 1, high)
+        case(arr:Array[Int]) if (arr(middle) > target) => search(arr, target, low, middle - 1)
+      }
+    }
+    val res = search(array, elem, 0, array.length - 1)
+    if (res==(-1)) false
+    else true
+  }
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = {
+    if (array.isEmpty) false
+    else if (array.head == elem) true
+    else linearSearch(array.tail, elem)
+  }
 
 }
--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -1,11 +1,51 @@
 package edu.knoldus
 
+import scala.util.control.Breaks.{break, breakable}
+
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = ???
+  def insertionSort(array: Array[Int]): Array[Int] = {
+          for ( j <- 1 until array.length ) {
+            breakable {
+              for ( i <- (1 to j).reverse ) {
+                if (array(i-1) < array(i)) {
+                  break
+                } else {
+                  val temp = array(i)
+                  array(i) = array(i-1)
+                  array(i-1) = temp
+                }
+              }
+            }
+          }
+        array
+  }
 
-  def selectionSort(array: Array[Int]): Array[Int] = ???
 
-  def bubbleSort(array: Array[Int]): Array[Int] = ???
+  def selectionSort(array: Array[Int]): Array[Int] = {
+      def selSort(arr: Array[Int], i1: Int, i2: Int) = {
+          val temp = arr(i1)
+          arr(i1) = arr(i2)
+          arr(i2) = temp
+      }
+    for (i <- 0 until array.size - 1)
+      selSort(array, i, (i + 1 until array.size).foldLeft(i)((currMin, index) =>
+          if (array(index) < array(currMin)) index
+          else currMin)
+          )
+    array
+  }
 
-}
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    for(i <- 0 until array.length-1){
+      for(j <- 0 until array.length-1-i){
+        if(array(j)>array(j + 1)){
+          val temp = array(j)
+          array(j) = array(j + 1)
+          array(j + 1) = temp
+         }
+        }
+      }
+    array
+    }
+  }
